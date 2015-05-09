af706d5b8bee8956fa6ea85a06b201620e3c7536
Stephen <stephenherko@gmail.com>
2015-05-09 12:25:10 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
-/********************
-
-Node Modules
-
-********************/
+/**
+ * Node Modules
+ */
 
 var gulp = require('gulp'),
     gulpUtil = require('gulp-util'),
@@ -10,18 +8,14 @@ var gulp = require('gulp'),
     minify = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     livereload = require('gulp-livereload'),
+    sass = require('gulp-sass'),
     watch = require('gulp-watch');
 
-/********************
+/**
+ * Tasks
+ */
 
-Tasks
-
-********************/
-
-// task: default
-gulp.task('default', ['watch']);
-
-// task: javascripts
+// task: scripts
 gulp.task('scripts', function(){
   return gulp.src('js/libs/*.js')
     .pipe(concat('main.min.js'))
@@ -29,24 +23,26 @@ gulp.task('scripts', function(){
     .pipe(gulp.dest('js/min'))
 });
 
-// task: css
-gulp.task('css', function(){
-  return gulp.src('css/libs/*.css')
+// task: sass
+gulp.task('sass', function () {
+  gulp.src('./scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(concat('main.min.css'))
     .pipe(minify())
-    .pipe(gulp.dest('css/min'))
+    .pipe(gulp.dest('./css'));
 });
 
-/********************
+/**
+ * Watch
+ */
 
-Watch
-
-********************/
+// task: default
+gulp.task('default', ['watch']);
 
 // watch over changes of source files
 gulp.task('watch', function() {
   gulp.watch('./').on('change', livereload.changed);
   gulp.watch('./js/**', ['scripts']);
-  gulp.watch('./css/**', ['css']);
+  gulp.watch('./scss/**', ['sass']);
   gulp.watch('./css/**').on('change', livereload.changed);
 });
